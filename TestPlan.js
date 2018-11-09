dd8fc828c0eca4002fc1a3a630e1a578bbcd2b46
alavita11 <44453029+alavita11@users.noreply.github.com>
2018-11-08 21:41:36 -0500

    Add files via upload

---
+++ TestPlan.js
@@ -0,0 +1,54 @@
+describe('Auto Loan Calculator', function () {
+
+  it('Validates URL', function () {
+    cy.visit('https://www.bankrate.com/calculators/auto/auto-loan-calculator.aspx')
+    cy.url().should('eq', 'https://www.bankrate.com/calculators/auto/auto-loan-calculator.aspx') 
+  })
+
+  it('Checks the amortization schedule', function () {
+    cy.contains('See amortization schedule').click()
+
+  })
+
+    
+  it('Enters borrow amount', function () {
+     cy.get('gearbox-input') //[name='amount']
+     .type('100001')
+     .should('have.value', '100001')
+  //.expect('br-amount form-input --data-format-left --has-error').to.be.true
+    //https://screencast.com/t/TCCDZ8bJUa
+
+  })
+
+    it('Selects item from drowndown', function () {
+      cy.get('dropdown__selected selected selectedTitle +mg-bottom-none').click()
+    //.expect('dropdown__selected selected selectedTitle +mg-bottom-none --is-active').to.be.true
+      .select('dropdown__input-label').contains('48 months')
+      .expect('dropdown__list-item selected').to.be.true
+    
+    cy.get('dropdown__selected selected selectedTitle +mg-bottom-none').click()
+    //.expect('dropdown__selected selected selectedTitle +mg-bottom-none --is-active').to.be.true
+      .select('dropdown__input-label').contains('60 months')
+      .expect('dropdown__list-item selected').to.be.true
+      
+    cy.get('dropdown__selected selected selectedTitle +mg-bottom-none').click()
+    //.expect('dropdown__selected selected selectedTitle +mg-bottom-none --is-active').to.be.true    
+      .select('dropdown__input-label').contains('72 months')
+      .expect('dropdown__list-item selected').to.be.true
+      
+    cy.get('dropdown__selected selected selectedTitle +mg-bottom-none')
+    //.expect('dropdown__selected selected selectedTitle +mg-bottom-none --is-active').to.be.true
+      .select('dropdown__input-label').contains('New')
+      .expect ('dropdown__list-item selected').to.be.true
+    })
+    
+    it('Enters an intererest rate', function () {
+    cy.get('gearbox-input').contains('interest-rate')
+    .type('99')
+    .expect('error-message interest-input__error').contains('Invalid rate')
+      
+
+    
+
+  })
+})
